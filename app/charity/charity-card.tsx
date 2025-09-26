260a409905afdcb0aca482bc8a5ccbd739534c90
SerhiiCherkasov <42519720+SerhiiCherkasov@users.noreply.github.com>
2025-09-26 12:46:35 +0300

    review changes

--- app/charity/charity-card.tsx
+++ app/charity/charity-card.tsx
@@ -1,24 +1,20 @@
 import Image from 'next/image';
 
-export type CharityItem = {
+export type CharityCardPropsType = {
   image: string;
-  header: string[];
+  header: string;
   text: string[];
   link: string;
 };
 
-export const CharityCard = ({ image, header, text = [], link }: CharityItem) => {
+export const CharityCard = ({ image, header, text = [], link }: CharityCardPropsType) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-2.5" key={header[0]}>
       <a href={link} target="_blank">
         <Image src={image} width={270} height={270} alt={'image'}></Image>
       </a>
       <a href={link} target="_blank">
-        {header.map((line, index) => (
-          <h3 key={index} className="text-center !text-[22px] !font-bold">
-            {line}
-          </h3>
-        ))}
+        <h3 className="text-center !text-[22px] !font-bold">{header}</h3>
       </a>
       {text.map((line, index) => (
         <p key={index} className="text-center text-sm">
@@ -27,7 +23,7 @@ export const CharityCard = ({ image, header, text = [], link }: CharityItem) => 
       ))}
       <a href={link} target="_blank">
         <button className="border-3 border-white bg-black px-8 py-3.5 text-white transition-colors duration-300 ease-in-out hover:border-black hover:bg-white hover:text-black">
-          <p className="text-base font-bold">ПІДТРИМАТИ</p>
+          <p className="cursor-pointer text-base font-bold">ПІДТРИМАТИ</p>
         </button>
       </a>
     </div>
