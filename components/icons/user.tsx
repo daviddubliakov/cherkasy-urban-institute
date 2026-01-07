3d392b50911ac7c085b75b90803adef7e0110050
mikekvochko <mike.kvochko@n3wnormal.com>
2026-01-07 13:13:55 +0200

    add more icons

---
+++ components/icons/user.tsx
@@ -0,0 +1,23 @@
+import { type SVGProps } from 'react';
+
+type UserIconProps = SVGProps<SVGSVGElement>;
+
+export function UserIcon({ className, ...props }: UserIconProps) {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+      />
+    </svg>
+  );
+}
+
